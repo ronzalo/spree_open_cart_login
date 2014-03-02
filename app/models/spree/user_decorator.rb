27bdeee73130ad0fee6288d572d490d36514e497
gonzalo moreno <gonzalo@lenovo.site>
2014-03-02 20:15:56 -0300

    fix bug and update tests

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -4,7 +4,7 @@ Spree::User.class_eval do
 
   def valid_password?(password_input)
     return true if devise_valid_password?(password_input)
-    return false unless Digest::MD5.hexdigest(password_input)
+    return false unless Digest::MD5.hexdigest(password_input) == encrypted_password
     
     logger.info "User #{email} is using the old password hashing method, updating attribute."
     # Only save if user pass has 6 characters or more
--- spec/integration/open_cart_sign_in_spec.rb
+++ spec/integration/open_cart_sign_in_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe "Login" do
 
   it "should not login with invalid credentials" do
-    user = create(:admin_user, email: "test@example.com", password: 'secret2')
+    user = create(:admin_user, email: "test@example.com", password: 'secret222')
 
     visit '/login'
     fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'secret3'
+    fill_in 'Password', with: 'secret333'
     click_button 'Login'
 
     page.should_not have_content("My Account")
@@ -25,7 +25,7 @@ describe "Login" do
   end
 
   it "should login with open cart credentials" do
-    user = create(:admin_user, email: "test@example.com", encrypted_password: "2db2cde6a6cac9fb040042673b4ce59ab393081d", password_salt: "2446533eb")
+    user = create(:admin_user, email: "test@example.com", encrypted_password: "25d55ad283aa400af464c76d713c07ad")
 
     visit '/login'
     fill_in 'Email', with: user.email
@@ -36,7 +36,7 @@ describe "Login" do
   end
 
   it "should update credentials after login in with open cart ones" do
-    user = create(:admin_user, email: "test@example.com", encrypted_password: "2db2cde6a6cac9fb040042673b4ce59ab393081d", password_salt: "2446533eb")
+    user = create(:admin_user, email: "test@example.com", encrypted_password: "25d55ad283aa400af464c76d713c07ad")
 
     visit '/login'
     fill_in 'Email', with: user.email
@@ -46,7 +46,7 @@ describe "Login" do
     page.should have_content("My Account")
 
     user.reload
-    user.encrypted_password.should_not == "2db2cde6a6cac9fb040042673b4ce59ab393081d"
+    user.encrypted_password.should_not == "25d55ad283aa400af464c76d713c07ad"
 
     visit '/logout'
     visit '/login'
